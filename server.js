5d9517b727c9b026629c638b2558da8a4613de9e
Jerlin2465 <jerlinjack246@gmail.com>
2026-06-30 16:06:52 +0530

    FEAT:change

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5000",
       "http://localhost:5001",
-      "https://jerry-restaurant.vercel.app/",
+      "https://jerry-restaurant.vercel.app",
       "https://restaurant-backend-tttl.onrender.com",
     ],
     credentials: true,
