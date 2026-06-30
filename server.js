7cd2e204669a60dee55d1b38772770e44663d471
Jerlin2465 <jerlinjack246@gmail.com>
2026-06-30 16:29:44 +0530

    FEAT:change

--- server.js
+++ server.js
@@ -46,7 +46,8 @@ app.use("/table", tableBookingRouter);
 app.use("/food", foodorderRouter);
 app.use("/bill", billRouter);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
